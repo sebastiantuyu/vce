6f2cbff34d3cb859c5e59dac4e8c3d9c5b5a4f75
sebastiantuyu <alextpineiro@gmail.com>
2021-02-01 09:43:27 -0600

    Adding initial files

---
+++ ControlPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlEscolar
+{
+    public partial class ControlPrincipal : Form
+    {
+        public ControlPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
